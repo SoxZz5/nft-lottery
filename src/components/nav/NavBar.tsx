53566838e92c933171d974476e4e4e9edec5daf1
LG <lgiffard44@gmail.com>
2021-11-29 03:16:33 +0100

    fix: Fix team menu not showed

--- src/components/nav/NavBar.tsx
+++ src/components/nav/NavBar.tsx
@@ -17,7 +17,7 @@ const NavBar: React.FunctionComponent = () => {
     { label: "Mint", id: "#minter" },
     //{ label: "Hangar", id: "#hangar" },
     { label: "Roadmap", id: "#roadmap" },
-    //{ label: "Team", id: "#team" },
+    { label: "Team", id: "#team" },
     { label: "White Paper", id: "" },
   ];
   const isActive = (menu: string): any => {
