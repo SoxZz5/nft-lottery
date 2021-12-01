fd0f5101f2500986be8f61408f7ad8a3a92cb46a
KonyTech <nicolas.cortella@protonmail.com>
2021-12-01 16:01:50 +0100

    feat: Add IPFS CAR generation for images

---
+++ src/scripts/ipfs/1_generate_json.ts
@@ -0,0 +1,10 @@
+const collectionMetadata = {
+    "name": "SpaceShips Season 1",
+    "description": "Aliens are taking over. Get a spaceship and join the army!",
+    "image": "https://bafybeibha4i5mjblupky6cvkq2cm5h3wvpiki7ng5l37h34yth352pls5y.ipfs.dweb.link/collection-logo.png",
+    "external_link": "https://spaceships-externallink.io",
+    "seller_fee_basis_points": 100,
+    "fee_recipient": "0xf585378ff2A1DeCb335b4899250b83F46DC5c019"
+};
+
+export {}
